3c4223de60a347cf240b76ec9ac5c0a21bb5a6de
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-10-31 15:11:05 -0300

    feat: add vite plugin pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'icons/apple-touch-icon.png',
+        'icons/maskable_icon.png'
+      ],
+      manifest: {
+        name: 'QShift',
+        short_name: 'QShift',
+        theme_color: '#2196f3',
+        background_color: '#2196f3',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/maskable_icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/icons/logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
